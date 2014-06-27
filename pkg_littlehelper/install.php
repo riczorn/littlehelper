7ddb6d7ab06ea1b55be066c9ef1ebcc58c4fe03c
Riccardo Zorn <support@tmg.it>
2014-06-27 15:48:07 +0200

    Version 2.0.16

--- pkg_littlehelper/install.php
+++ pkg_littlehelper/install.php
@@ -30,7 +30,7 @@ class pkg_littlehelperInstallerScript
 			$manifest = JPATH_ADMINISTRATOR.'/components/com_littlehelper/littlehelper.xml';
 			if (file_exists($manifest)) {
 				error_log('manifest exists');
-				$reg = JRegistry::getInstance();
+				$reg = new JRegistry();
 				$reg->loadFile($manifest);
 				error_log('found version '.$reg->get('version'));
 
